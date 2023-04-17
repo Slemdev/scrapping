9b138c50610a3042eb5488d5e5edecb0ab46a27e
Ne'evaiti <52328785+neevaiti@users.noreply.github.com>
2023-04-17 11:16:05 +0200

    feat: try to get links, with description

--- imdb_scrap/imdb_scrap/spiders/get_top_movies_spider.py
+++ imdb_scrap/imdb_scrap/spiders/get_top_movies_spider.py
@@ -9,16 +9,7 @@ class GetTopMoviesSpider(scrapy.Spider):
     def parse(self, response):
         movies = response.css(".titleColumn a::text").extract()
         casting = response.css(".titleColumn a::attr(title)").extract()
-        yield {"movies": movies, "casting": casting}
-        pass
-        
-
-class GetDescriptionTopMoviesSpider(scrapy.Spider):
-    name = "get_description_top_movies"
-    allowed_domains = ["imdb.com"]
-    start_urls = ["https://www.imdb.com/chart/top/?ref_=nv_mv_250"]
-
-    def parse(self, response):
-        descriptions = response.css(".ratingColumn.imdbRating p::text").extract()
-        yield {"descriptions": descriptions}
+        rating = response.css(".ratingColumn.imdbRating strong::text").extract()
+        links = response.css(".titleColumn a::attr(href)").extract()
+        yield {"movies": movies, "casting": casting, "rating": rating, "links": links}
         pass
