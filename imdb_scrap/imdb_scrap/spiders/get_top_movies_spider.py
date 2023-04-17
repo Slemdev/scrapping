4dc700edb883412eae797303bfc330b9f0d1d55f
Slemdev <lemairestephanemail@gmail.com>
2023-04-17 15:42:10 +0200

    maj

--- imdb_scrap/imdb_scrap/spiders/get_top_movies_spider.py
+++ imdb_scrap/imdb_scrap/spiders/get_top_movies_spider.py
@@ -3,6 +3,29 @@ from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 from ..items import ImdbScrapItem
 
+class GetTopMoviesSpider(scrapy.Spider):
+    name = "get_top_movies"
+    allowed_domains = ["imdb.com"]
+    start_urls = ["https://www.imdb.com/chart/top/?ref_=nv_mv_250"]
+
+    def parse(self, response):
+        items = ImdbScrapItem()
+        all_movies = response.css(".lister-list tr")
+        
+        for movie in all_movies:
+            title = response.css(".titleColumn a::text").extract()
+            casting = response.css(".titleColumn a::attr(title)").extract()
+            rating = response.css(".ratingColumn.imdbRating strong::text").extract()
+            link = movie.css("a::attr(href)").extract_first()
+            
+            items['title'] = title
+            items['casting'] = casting
+            items['rating'] = rating
+            items['link'] = link
+            
+            yield items
+            pass
+        
 
 class CrawlerGetTopMoviesSpider(CrawlSpider):
     name = 'crawler_get_top_movies'
@@ -10,7 +33,7 @@ class CrawlerGetTopMoviesSpider(CrawlSpider):
     user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'
     
     rules = (
-        Rule(LinkExtractor(allow=(), restrict_xpaths=('//tbody[@class="lister-list"]/tr/td[@class="titleColumn"]',)), callback="parse_item", follow=True),
+        Rule(LinkExtractor(allow=(), restrict_xpaths=('//tbody[@class="lister-list"]/tr/td[@class="titleColumn" ]',)), callback="parse_item", follow=True),
     )
 
     def start_requests(self):
@@ -38,25 +61,3 @@ class CrawlerGetTopMoviesSpider(CrawlSpider):
         yield items
 
 
-class GetTopMoviesSpider(scrapy.Spider):
-    name = "get_top_movies"
-    allowed_domains = ["imdb.com"]
-    start_urls = ["https://www.imdb.com/chart/top/?ref_=nv_mv_250"]
-
-    def parse(self, response):
-        items = ImdbScrapItem()
-        all_movies = response.css(".lister-list tr")
-        
-        for movie in all_movies:
-            title = response.css(".titleColumn a::text").extract()
-            casting = response.css(".titleColumn a::attr(title)").extract()
-            rating = response.css(".ratingColumn.imdbRating strong::text").extract()
-            link = movie.css("a::attr(href)").extract_first()
-            
-            items['title'] = title
-            items['casting'] = casting
-            items['rating'] = rating
-            items['link'] = link
-            
-            yield items
-            pass
